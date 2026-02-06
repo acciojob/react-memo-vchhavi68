59c415ce38cd90d6d71ccbdfc79f00c4abe0369d
vchhavi68 <vchhavi68@gmail.com>
2026-02-06 21:43:51 +0530

    Update ReactMemo.js

--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -1,1 +1,46 @@
+import React from "react";
 
+const ReactMemo = () => {
+
+    const [skill,setSkill] = React.useState("");
+    const [skills,setSkills] = React.useState(["HTML","CSS","JavaScript","React"])
+
+    const [error,setError] = React.useState("")
+
+    const handleChangeInput = (e) => {
+        if(e.target.value.length<=5){
+            setError("The length must be greater than 5")
+        }else{
+            setError("")
+        }
+        setSkill(e.target.value)
+    }
+
+    const handleAddSkill = () => {
+        setSkills(skills.concat(skill))
+    }
+    return(
+        <>
+            <input id="skill-input" type="text" onChange={handleChangeInput}/>
+            {
+                error.length ? <small id="small">{error}</small> :null
+            }
+            <button id="skill-btn" onClick={handleAddSkill}>Add Skill</button>
+            <SkillList skills={skills} />
+        </>
+    )
+}
+const SkillList = React.memo(({skills}) => {
+    console.log("rerendering")
+    return(
+        <ul>
+            {
+                skills.map((skill,i)=>(<li id={`item-${skill}`} key={i}>{skill}</li>))
+            }
+        </ul>
+    )
+})
+
+
+
+export default ReactMemo;
